aae8ea8862c8dd2b31f15eae08519ba4fbda7007
321riyaroy123 <riyaroymct2018@gmail.com>
2025-09-13 19:33:21 +0530

    Update App.jsx

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import './LoginPage.css';
+import './App.css';
 
 import Login from "./Login";
 import Register from "./Register";
