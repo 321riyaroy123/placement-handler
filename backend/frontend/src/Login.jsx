e4a142bf56cc256502abac9cf65b3c11dd195e6f
321riyaroy123 <riyaroymct2018@gmail.com>
2025-09-13 19:48:44 +0530

    Update Login.jsx

--- backend/frontend/src/Login.jsx
+++ backend/frontend/src/Login.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://placement-handler.onrender.com/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
@@ -168,3 +168,4 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
+
